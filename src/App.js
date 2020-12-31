d715951b0b06e9dde3129d376931a56b22c4bfad
Cecile Leung-Tack <cecile.leungtack@gmail.com>
2020-12-31 13:30:21 -0500

    CLT : routing et connexion du frontend à l'API

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import React, {useState} from "react";
 import Tweet from "./Tweet";
+import Nav from './Nav';
+import Shop from './Shop';
+import About from './About'
+import {BrowserRouter as Router,  Switch, Route} from 'react-router-dom';
 
 function App() {
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-  const [users, setUsers] = useState([
-    {username: "ed", password: "mdp"},
-    {username: "cecile", password: "mdp2"}
-    ]);
-
-  const increment = () => {
-    setCount(count+1);
-    setRed(!isRed);
-  }
-
-  const sayHello = () => {
-    console.log('hello');
-  };
   const counter = 0;
   return(
-    <div className='app'>
-      {users.map (user => (
-        <Tweet name={user.username} message={user.password}/>
-      ))}
-      <h1 className={isRed ? 'red' : ''}>Change my color</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
-      <Tweet  name='Cecile' message='This is a random tweet'/>
-      <Tweet name='Helo' message='Mosh is the best'/>
-    </div>
+    <Router>
+      <div className='app'>
+        <Nav />
+        <Switch>
+          <Route path='/about' exact component = {About}/>
+          <Route path='/shop' exact component = {Shop}/>
+        </Switch>
+      </div>
+    </Router>
   );
-
 }
+
+
 
 export default App;
