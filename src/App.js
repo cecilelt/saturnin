ae5541b9ff5e7cd2d0abbf35b43620ede5bc3de4
Cecile Leung-Tack <cecile.leungtack@gmail.com>
2020-12-31 11:31:48 -0500

    CLT : tutoriel props et components (tweets)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Tweet from "./Tweet";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([
+    {username: "ed", password: "mdp"},
+    {username: "cecile", password: "mdp2"}
+    ]);
+
+  const increment = () => {
+    setCount(count+1);
+    setRed(!isRed);
+  }
+
+  const sayHello = () => {
+    console.log('hello');
+  };
+  const counter = 0;
+  return(
+    <div className='app'>
+      {users.map (user => (
+        <Tweet name={user.username} message={user.password}/>
+      ))}
+      <h1 className={isRed ? 'red' : ''}>Change my color</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+      <Tweet  name='Cecile' message='This is a random tweet'/>
+      <Tweet name='Helo' message='Mosh is the best'/>
     </div>
   );
+
 }
 
 export default App;
